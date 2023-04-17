a4c4f3788a694c4f71e90db10c45becd3ef99d79
Salami Oluwadamilola <105321247+TiminiOfLagos@users.noreply.github.com>
2023-04-17 15:05:38 +0100

    Update java.js

--- java.js
+++ java.js
@@ -15,14 +15,14 @@ let weather = {
         const { icon, description } = data.weather[0];
         const { temp, humidity } = data.main;
         const { speed } = data.wind;
-        document.querySelector(".city").innerText = "Weather in" + name;
+        document.querySelector(".city").innerText = " Weather in" + name;
         document.querySelector(".icon").src = 
         "http://openweathermap.org/img/wn/" + icon + ".png";
         document.querySelector(".description").innerText = description;
         document.querySelector(".temp").innerText = temp + "°C";
         document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
         document.querySelector(".wind").innerText = "Wind speed: " + speed + "km/hr";
-        document.querySelector(".weather").classList.remove("loading");
+        document.querySelector(".weather").classList.remove(" loading ");
         document.body.style.backgroundImage = "url('https://source.unsplash.com/1600x900/?" + name + "')";
 
     },
